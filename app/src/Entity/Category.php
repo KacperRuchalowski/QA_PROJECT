07da15dd966ffe3e835bb8a71714084008545642
Kacper Ruchałowski <kacperruch@gmail.com>
2020-06-26 12:13:44 +0200

    Added visuals and translations

--- app/src/Entity/Category.php
+++ app/src/Entity/Category.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Form\Extension\Validator\Constraints;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
